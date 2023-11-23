29eb0f36d68cfffa2df35bb1f293697d90d72867
SheepDoctor <12211111@mail.sustech.edu.cn>
2023-11-23 14:18:15 +0800

    0.0.4

---
+++ HDL_Framework/GenshinKitchen.srcs/sources_1/new/Input_Module.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/11/23 14:17:04
+// Design Name: 
+// Module Name: Input_Module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Input_Module(
+
+    );
+endmodule
