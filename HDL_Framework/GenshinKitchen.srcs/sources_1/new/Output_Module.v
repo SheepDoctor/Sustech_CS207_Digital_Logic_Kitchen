c64479c12591f513250e242db8219442aee0ba02
mapleZhu <151593811+mapleZhu@users.noreply.github.com>
2023-12-15 21:34:02 +0800

    Update Output_Module.v

--- HDL_Framework/GenshinKitchen.srcs/sources_1/new/Output_Module.v
+++ HDL_Framework/GenshinKitchen.srcs/sources_1/new/Output_Module.v
@@ -24,13 +24,11 @@ module Output(
     input clk,
     input [7:0] switches,
     input [4:0] button,
-    input dataIn_ready,
     input rst_n,
-    input [7:0] dataOut_bits,
-    input dataOut_valid,
+    input [3:0] led,
     output reg [7:0]dataIn_bits
 );
-parameter S_start = 3'b000,S_end = 3'b001,S_action = 3'b010;
+parameter S_start = 3'b000,S_end = 3'b001,S_action = 3'b010,S_target = 3'b011;
 reg [2:0] state;
 reg [2:0] n_state;
 always @(posedge clk, negedge rst_n) begin
@@ -55,13 +53,20 @@ else n_state = S_action;
 S_action:
 begin
 if(switches[7]&~switches[6]) {n_state,dataIn_bits} = {S_end,8'b0000_1001};
-else if(button[0]) {n_state,dataIn_bits} = {S_action,8'b0000_0110};
-else if(button[1]) {n_state,dataIn_bits} = {S_action,8'b0000_1010};
-else if(button[2]) {n_state,dataIn_bits} = {S_action,8'b0001_0010};
-else if(button[3]) {n_state,dataIn_bits} = {S_action,8'b0010_0010};
-else if(button[4]) {n_state,dataIn_bits} = {S_action,8'b0100_0010};
-else if(switches[5:0]>20) {n_state,dataIn_bits} = {S_action,8'b0000_0000};
-else {n_state,dataIn_bits} = {S_action,switches[5:0],2'b11};
+else if(led[0]&led[1]&~led[3]&button[0]) {n_state,dataIn_bits} = {S_action,8'b0000_0110};//get
+else if(led[0]&led[1]&button[1]) {n_state,dataIn_bits} = {S_action,8'b0000_1010};//put
+else if(button[0]) {n_state,dataIn_bits} = {S_action,8'b0001_0010};//move
+else if(led[0]&button[3]) {n_state,dataIn_bits} = {S_action,8'b0010_0010};//interact
+else if((button==9|button==11|button==14|button==17|button==19|button==20)&led[1]&button[4]) {n_state,dataIn_bits} = {S_action,8'b0100_0010};//throw
+else {n_state,dataIn_bits} = {S_target,8'b0000_0010};
+end
+
+S_target:
+begin
+if(switches[7]&~switches[6]) {n_state,dataIn_bits} = {S_end,8'b0000_1001};
+else if(switches[5:0]>20) {n_state,dataIn_bits} = {S_target,8'b0000_0011};
+else if(button>0) n_state = S_action;
+else {n_state,dataIn_bits} = {S_target,switches[5:0],2'b11};
 end
 
 endcase
