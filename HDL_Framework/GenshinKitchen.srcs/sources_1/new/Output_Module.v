8c08369b26568da8f794b7ab1373d8fb16d6bb8f
mapleZhu <151593811+mapleZhu@users.noreply.github.com>
2023-12-31 23:11:48 +0800

    add footnotes

--- HDL_Framework/GenshinKitchen.srcs/sources_1/new/Output_Module.v
+++ HDL_Framework/GenshinKitchen.srcs/sources_1/new/Output_Module.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+`include "constants.vh"
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -19,22 +20,29 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-// control the beginning and the ending of the game
+/*
+this module aims to output dataIn_bits(dataIn_user)
+notes:
+for led[3:0]
+led[0]: in front of the target machine
+led[1]:
+led[2]:
+led[3]: there is item in target machine
+*/
 module Output(
     input clk,
     input [7:0] switches,
     input [4:0] button,
     input rst_n,
-    input [3:0] led,
-    output reg [7:0]dataIn_bits
+    input [3:0] led,// signal for judging legality 
+    output reg [7:0]dataIn_bits // dataIn_user
 );
-parameter S_start = 3'b000,S_end = 3'b001,S_action = 3'b010,S_target = 3'b011;
 reg [2:0] state;
 reg [2:0] n_state;
 
 always @(posedge clk, negedge rst_n) begin
     if(~rst_n) begin
-    state <= S_end; 
+    state <= `S_end; 
     end
     else
     state <= n_state;
@@ -43,38 +51,38 @@ end
 always@(state,switches,button) begin
 case(state)
 
-S_end:
-if(~switches[7]&switches[6]) {n_state,dataIn_bits} = {S_start,8'b0000_0101};
-else {n_state,dataIn_bits} = {S_end,8'b0000_1001};
+`S_end:// game not start (initial state)
+if(~switches[7]&switches[6]) {n_state,dataIn_bits} = {`S_start,8'b0000_0101};// game start
+else {n_state,dataIn_bits} = {`S_end,8'b0000_1001};
 
-S_start:
-if(switches[7]&~switches[6]) {n_state,dataIn_bits} = {S_end,8'b0000_1001};
-else if((~switches[7]&switches[6])|(switches[7]&switches[6])) {n_state,dataIn_bits} = {S_start,8'b0000_0101};
-else n_state = S_action;
+`S_start:// enter this state after outputing the game start signal, in this state, there will be no reaction when changing switches or buttons
+if(switches[7]&~switches[6]) {n_state,dataIn_bits} = {`S_end,8'b0000_1001};// game end
+else if((~switches[7]&switches[6])|(switches[7]&switches[6])) {n_state,dataIn_bits} = {`S_start,8'b0000_0101};
+else n_state = `S_action; // when switches[7:6] = 2'b00, change state
 
-S_action:
+`S_action:// state for get/put/interact/move/throw
 begin
-if(switches[7]&~switches[6]) {n_state,dataIn_bits} = {S_end,8'b0000_1001};
+if(switches[7]&~switches[6]) {n_state,dataIn_bits} = {`S_end,8'b0000_1001};// game end
 
-else if(led[0]&~led[1]&led[3]&button[0]) {n_state,dataIn_bits} = {S_action,8'b0000_0110};//get
+else if(led[0]&~led[1]&led[3]&button[0]) {n_state,dataIn_bits} = {`S_action,8'b0000_0110};//judge: if in front of the machine and no item in hand and machine has item, get
 
-else if(switches[5:0]>6&led[0]&led[1]&button[1]) begin
-if(~led[3]) {n_state,dataIn_bits} = {S_action,8'b0000_1010};//put
-else if(switches==9|switches==10|switches==11|switches==12|switches==13|switches==14|switches==15|switches==16|switches==17|switches==19) {n_state,dataIn_bits} = {S_action,8'b0000_1010};
+else if(switches[5:0]>6&led[0]&led[1]&button[1]) begin // judge: legal target machine and in front of the machine and has item in hand
+if(~led[3]) {n_state,dataIn_bits} = {`S_action,8'b0000_1010};// if there is no item in target machine, put
+else if(switches==9|switches==10|switches==11|switches==12|switches==13|switches==14|switches==15|switches==16|switches==17|switches==19) {n_state,dataIn_bits} = {`S_action,8'b0000_1010};// if target machine is among these machines, put
 end
 
-else if(led[0]&button[2]) {n_state,dataIn_bits} = {S_action,8'b0001_0010};//interact
-else if(button[3]) {n_state,dataIn_bits} = {S_action,8'b0010_0010};//move
-else if((switches[5:0]==9|switches[5:0]==11|switches[5:0]==14|switches[5:0]==17|switches[5:0]==19|switches[5:0]==20)&led[1]&button[4]) {n_state,dataIn_bits} = {S_action,8'b0100_0010};//throw
-else {n_state,dataIn_bits} = {S_target,8'b0000_0010};
+else if(led[0]&button[2]) {n_state,dataIn_bits} = {`S_action,8'b0001_0010};//judge: if in front of the machine interact
+else if(button[3]) {n_state,dataIn_bits} = {`S_action,8'b0010_0010};// move(no need for judge)
+else if((switches[5:0]==9|switches[5:0]==11|switches[5:0]==14|switches[5:0]==17|switches[5:0]==19|switches[5:0]==20)&led[1]&button[4]) {n_state,dataIn_bits} = {`S_action,8'b0100_0010};// judge: if target machine is among these machines and has item in hand, throw
+else {n_state,dataIn_bits} = {`S_target,8'b0000_0010};// reset signal in 10 channel, otherwise, you can't double move/put/get/interact/throw
 end
 
-S_target:
+`S_target:// state for choosing target machine
 begin
-if(switches[7]&~switches[6]) {n_state,dataIn_bits} = {S_end,8'b0000_1001};
-else if(switches[5:0]>20) {n_state,dataIn_bits} = {S_target,8'b0000_0011};
-else if(button>0) n_state = S_action;
-else {n_state,dataIn_bits} = {S_target,switches[5:0],2'b11};
+if(switches[7]&~switches[6]) {n_state,dataIn_bits} = {`S_end,8'b0000_1001};// game end 
+else if(switches[5:0]>20) {n_state,dataIn_bits} = {`S_target,8'b0000_0011};// judge legality(the number of target machine can't be more than 20)
+else if(button>0) n_state = `S_action;
+else {n_state,dataIn_bits} = {`S_target,switches[5:0],2'b11};// choose target machine
 end
 
 endcase
