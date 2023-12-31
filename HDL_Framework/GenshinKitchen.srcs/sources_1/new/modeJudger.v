1a5d82277afa0bcf2056386dfcda3e9bdb3b13df
mapleZhu <151593811+mapleZhu@users.noreply.github.com>
2023-12-31 23:14:43 +0800

    add footnotes

--- HDL_Framework/GenshinKitchen.srcs/sources_1/new/modeJudger.v
+++ HDL_Framework/GenshinKitchen.srcs/sources_1/new/modeJudger.v
@@ -1,30 +1,33 @@
+`include "constants.vh"
+/*
+this module aims to judge the current mode
+*/
 module modeJudger(
-input [7:0]dataIn_bits,
+input [7:0]dataIn_bits,// *data from script module but not dataIn_bits*
 input clk,
 input rst_n,
-output reg switchMode
+output reg switchMode // show current mode
     );
 reg state;
 reg n_state;
-parameter user = 1'b1, script = 1'b0;
 always @(posedge clk, negedge rst_n) begin
         if(~rst_n) begin
-        state <= user; 
+        state <= `user; // initially, user mode
         end
         else
         state <= n_state;
 end
 always@(state,dataIn_bits) begin
 case(state)
-user:
+`user:
 casex(dataIn_bits)
-8'bxxxx_0101:{n_state,switchMode} = {script,1'b1};
-default:{n_state,switchMode} = {user,1'b0};
+8'bxxxx_0101:{n_state,switchMode} = {`script,1'b1}; // if script module sends begin signal, convert to script module
+default:{n_state,switchMode} = {`user,1'b0}; // otherwise, still user module
 endcase
-script:
+`script:
 casex(dataIn_bits)
-8'bxxxx_1001:{n_state,switchMode} = {user,1'b0};
-default:{n_state,switchMode} = {script,1'b1};
+8'bxxxx_1001:{n_state,switchMode} = {`user,1'b0}; // if script module sends end signal, convert to script module
+default:{n_state,switchMode} = {`script,1'b1}; // otherwise, still script module
 endcase
 endcase
 end
